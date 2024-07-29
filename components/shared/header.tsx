1e61b9b6dbbd7166c7d5bb4bc1f50eab72ce6d3b
Duracell27 <volodymyrshmidt@gmail.com>
2024-07-29 23:12:07 +0300

    nextPizza

---
+++ components/shared/header.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Container } from "./container";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { ArrowRight, ShoppingCart, User } from "lucide-react";
+
+type Props = {
+  className?: string;
+};
+
+const Header: React.FC<Props> = ({ className }) => {
+  return (
+    <header className={cn("border border-b", className)}>
+      <Container className="flex items-center justify-between py-8">
+        <div className="flex items-center gap-4">
+          <Image src={"/logo.png"} width={35} height={35} alt="logo" />
+          <div className="">
+            <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">taste as well</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant={"outline"} className="flex items-center gap-1">
+            <User size={16} />
+            Sign In
+          </Button>
+          <div className="">
+            <Button className="group relative">
+              <b>78 $</b>
+              <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+              <div className="flex items-center gap-1 duration-300 group-hover:opacity-0">
+                <ShoppingCart className="s-4 relative">
+                  <b>3</b>
+                </ShoppingCart>
+              </div>
+              <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
+            </Button>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
