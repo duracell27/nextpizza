9b1595921506e54f088a3687fe8beb45ed4d29e6
Duracell27 <volodymyrshmidt@gmail.com>
2024-08-02 17:54:00 +0300

    5.04

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -30,9 +30,8 @@ export const Header: React.FC<Props> = ({ className }) => {
               <b>78 $</b>
               <span className="h-full w-[1px] bg-white/30 mx-3"></span>
               <div className="flex items-center gap-1 duration-300 group-hover:opacity-0">
-                <ShoppingCart className="s-4 relative">
-                  <b>3</b>
-                </ShoppingCart>
+                <ShoppingCart className="s-4 relative"></ShoppingCart>
+                <b>3</b>
               </div>
               <ArrowRight className="w-5 absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0" />
             </Button>
@@ -42,4 +41,3 @@ export const Header: React.FC<Props> = ({ className }) => {
     </header>
   );
 };
-
